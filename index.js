40fc8ef18f00fd6db4a1e305b17f0d321621ba8b
tcase360 <tmcase16@gmail.com>
2017-06-08 16:43:19 +0200

    add node.js server and routing

---
+++ index.js
@@ -0,0 +1,21 @@
+let express = require('express');
+let app = express();
+
+let port = process.env.PORT || 8000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
